bb3de0a82f1a8bb62382fec1273b84bd76cb3ad9
Peti <Peti@Peti-PC>
2021-03-03 13:26:33 +0100

    4. gyakorlat 1. feladat 'C' verzió

--- 1. feladat/src/MyClasses/Employee.java
+++ 1. feladat/src/MyClasses/Employee.java
@@ -4,15 +4,18 @@ public class Employee {
 	private String name;
 	private int salary;
 	
+	// a)
 	public void increaseSalary(int value) {
 		salary += value;
 	}
 	
 	public String displayInfo() {
 		//increaseSalary(100);
-		return "Név: " + name + ", fizetés: " + salary;
+		return "Név: " + name + ", fizetés: " + salary +
+				", adó: " + getTax();
 	}
-
+	
+	// b)
 	public String getName() {
 		return name;
 	}
@@ -29,4 +32,28 @@ public class Employee {
 		if(salary > 0)
 			this.salary = salary;
 	}
+
+	// c)
+	public boolean isInRange(int lower, int upper) {
+		if(this.salary <= upper && this.salary >= lower)
+			return true;
+		return false;
+	}
+	
+	private double getTax() {
+		return Math.round(this.salary*0.16);
+	}
+	
+	public boolean hasHigherSalary(Employee e) {
+		if(this.salary > e.salary)
+			return true;
+		return false;
+	}
+	
+	public Employee compareSalary(Employee e) {
+		if(this.salary > e.salary)
+			return this;
+		return e;
+	}
+	
 }
